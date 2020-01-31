a931eb65988db05e12db741149327a724c08b8b7
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-01-31 08:13:24 +0700

    added stepper

---
+++ components/atoms/Stepper.tsx
@@ -0,0 +1,39 @@
+import { withTranslation, Router } from 'i18n';
+
+const Stepper = (props: any) => {
+  return (
+    <div className="c-stepper">
+      <div className="c-stepper__back">
+        <span className="icon-chevron_left" onClick={() => Router.back()} />
+      </div>
+      <div>
+        <div className="c-stepper__steps">
+          {`${props.t('step')} ${props.step} ${props.t('of')} ${props.totalSteps}`}
+        </div>
+        <div className="c-stepper__title">
+          <h1>{props.title}</h1>
+        </div>
+      </div>
+      <style jsx>{`
+        .c-stepper {
+          @apply flex items-end;
+          &__back {
+            @apply font-bold cursor-pointer;
+            margin-right: 12px;
+            line-height: 42px;
+          }
+          &__steps {
+            @apply font-opensans text-xs;
+          }
+          &__title {
+            @apply font-bold;
+            font-size: 28px;
+            line-height: 42px;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default withTranslation('common')(Stepper);
