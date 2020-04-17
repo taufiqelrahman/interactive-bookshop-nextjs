520be747a2fe71b652570ab95843be792b27fd4d
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-04-17 23:51:18 +0700

    fieldage

--- components/molecules/FieldAge.tsx
+++ components/molecules/FieldAge.tsx
@@ -3,17 +3,18 @@ import { withTranslation } from 'i18n';
 import Radio from 'components/atoms/Radio';
 import Badge from 'components/atoms/Badge';
 
-const FieldAge = React.forwardRef((props: any, ref: any) => (
-  <div>
-    <div className="c-field-age">
-      <div className="c-field-age__header">
-        {props.t('age-label')}
-        {props.errors && <Badge>!</Badge>}
-      </div>
-      <div className="c-field-age__options">
-        {/* dummy */}
-        {ref &&
-          [props.t('age-toddler'), props.t('age-kid')].map(age => (
+const FieldAge = React.forwardRef((props: any, ref: any) => {
+  const TODDLER = props.t('age-toddler') || 'Toddler';
+  const KID = props.t('age-kid') || 'Kid';
+  return (
+    <div>
+      <div className="c-field-age">
+        <div className="c-field-age__header">
+          {props.t('age-label')}
+          {props.errors && <Badge>!</Badge>}
+        </div>
+        <div className="c-field-age__options">
+          {[TODDLER, KID].map(age => (
             <Radio
               key={age}
               ref={ref}
@@ -25,24 +26,25 @@ const FieldAge = React.forwardRef((props: any, ref: any) => (
               defaultChecked={props.defaultChecked === age.toLowerCase()}
             />
           ))}
+        </div>
       </div>
+      <style jsx>{`
+        .c-field-age {
+          @apply mb-4;
+          @screen md {
+            @apply mb-0;
+          }
+          &__header {
+            @apply font-semibold mb-3 flex;
+          }
+          &__options {
+            @apply flex flex-wrap;
+          }
+        }
+      `}</style>
     </div>
-    <style jsx>{`
-      .c-field-age {
-        @apply mb-4;
-        @screen md {
-          @apply mb-0;
-        }
-        &__header {
-          @apply font-semibold mb-3 flex;
-        }
-        &__options {
-          @apply flex flex-wrap;
-        }
-      }
-    `}</style>
-  </div>
-));
+  );
+});
 FieldAge.displayName = 'FieldAge';
 
 export default withTranslation('form', { withRef: true })<any>(FieldAge);
