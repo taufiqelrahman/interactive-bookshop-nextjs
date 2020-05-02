58c2f0877215bc3f15837cbda338f40307ea4658
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-05-02 14:17:11 +0700

    updated book height mobile

--- components/atoms/BookPage.tsx
+++ components/atoms/BookPage.tsx
@@ -45,7 +45,12 @@ const BookPage = (props: any) => {
       <LazyLoad overflow>
         <svg className="c-book-page__svg">
           <foreignObject x="0" y="0" width="100%" height="100%">
-            <img className="c-book-page__image" src={props.mustLoad ? props.image : ''} alt="book page" />
+            <img
+              className="c-book-page__image"
+              src={props.mustLoad ? props.image : ''}
+              alt="book page"
+              style={props.isMobile ? { height: `calc((${props.height}))` } : {}}
+            />
             {props.contents.map((content, key) => {
               const value = processContent(content, props.language);
               return (
