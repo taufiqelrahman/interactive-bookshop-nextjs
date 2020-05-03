0e8407bf0f31a322d1f97431865610f8494a9cc9
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-05-03 09:52:11 +0700

    moved /static to /public/static

--- static/scripts/checkout.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
-  link.href = `${process.env.APP_URL}/static/favicon.ico`;
-  document.getElementsByTagName('head')[0].appendChild(link);
-})();
