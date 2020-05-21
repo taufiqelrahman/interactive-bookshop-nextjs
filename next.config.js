927d1d6615143f46d7a596fedb5e1c6442da8eca
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-05-21 15:53:28 +0700

    minimized css

--- next.config.js
+++ next.config.js
@@ -6,44 +6,52 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const withPWA = require('next-pwa');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = withCSS(
-  withPurgeCss(
-    withPWA({
-      webpack: config => {
-        config.plugins.push(
-          new Dotenv({
-            path: path.join(__dirname, '.env'),
-            systemvars: true,
-          }),
-        );
-        config.resolve.modules.push(path.resolve('./'));
-        // remove existing plugin
-        config.plugins = config.plugins.filter(plugin => {
-          return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin';
-        });
-        // only report errors on a matcher that doesn't match anything
-        config.plugins.push(
-          new ForkTsCheckerWebpackPlugin({
-            reportFiles: ['does-not-exist'],
-          }),
-        );
-        config.module.rules.push({
-          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
+module.exports = withBundleAnalyzer(
+  withCSS(
+    withPurgeCss(
+      withPWA({
+        webpack: config => {
+          config.plugins.push(
+            new Dotenv({
+              path: path.join(__dirname, '.env'),
+              systemvars: true,
+            }),
+          );
+          config.resolve.modules.push(path.resolve('./'));
+          // remove existing plugin
+          config.plugins = config.plugins.filter(plugin => {
+            return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin';
+          });
+          // only report errors on a matcher that doesn't match anything
+          config.plugins.push(
+            new ForkTsCheckerWebpackPlugin({
+              reportFiles: ['does-not-exist'],
+            }),
+          );
+          config.module.rules.push({
+            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 100000,
+              },
             },
-          },
-        });
-        return config;
-      },
-      pwa: {
-        disable: process.env.NODE_ENV !== 'production',
-        // dest: 'public/static',
-        publicExcludes: ['!static/images'],
-      },
-    }),
+          });
+          config.optimization.minimizer = [];
+          config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+          return config;
+        },
+        pwa: {
+          disable: process.env.NODE_ENV !== 'production',
+          // dest: 'public/static',
+          publicExcludes: ['!static/images'],
+        },
+      }),
+    ),
   ),
 );
