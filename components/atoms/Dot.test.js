86f327b1d2034ff983a57554a8b8092cd93e42b4
Taufiq El Rahman <taufiqelrahman65@gmail.com>
2020-06-29 00:57:20 +0700

    refactored unit test

--- components/atoms/Dot.test.js
+++ components/atoms/Dot.test.js
@@ -3,23 +3,25 @@ import { unmountComponentAtNode } from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import Dot from './Dot';
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
+describe('components/atoms/Dot', () => {
+  let container = null;
+  beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+  afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
 
-test('renders the component', () => {
-  render(<Dot color="red" />);
-  expect(screen).toMatchSnapshot();
-  render(<Dot />);
-  expect(screen).toMatchSnapshot();
+  it('renders the component', () => {
+    render(<Dot color="red" />);
+    expect(screen).toMatchSnapshot();
+    render(<Dot />);
+    expect(screen).toMatchSnapshot();
+  });
 });
