373618f0ef9f53839de01d204561e16865675ff4
Taufiq <taufiqelrahman65@gmail.com>
2025-08-29 21:26:49 +0700

    Refactor store configuration to replace 'default' with 'common' in combined reducers; enhance rootReducer for HYDRATE action handling

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,17 @@
+'use client';
+
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { createWrapper } from 'next-redux-wrapper';
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 
 import cartReducer from './cart/reducers';
 import masterReducer from './master/reducers';
 import ordersReducer from './orders/reducers';
 import productsReducer from './products/reducers';
-import reducer from './reducers';
+import commonReducer from './reducers'; // ganti default -> common
 import usersReducer from './users/reducers';
 
-const rootReducer = combineReducers({
-  default: reducer,
+const combinedReducer = combineReducers({
+  common: commonReducer,
   cart: cartReducer,
   orders: ordersReducer,
   products: productsReducer,
@@ -17,15 +19,33 @@ const rootReducer = combineReducers({
   master: masterReducer,
 });
 
+const rootReducer = (state: ReturnType<typeof combinedReducer> | undefined, action: any) => {
+  if (action.type === HYDRATE) {
+    return {
+      ...state,
+      ...action.payload,
+    };
+  } else {
+    return combinedReducer(state, action);
+  }
+};
+
 export const makeStore = () =>
   configureStore({
     reducer: rootReducer,
     devTools: process.env.NODE_ENV !== 'production',
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }), // supaya aman kalau ada non-serializable (misal Date)
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
   });
 
+// Types
 export type AppStore = ReturnType<typeof makeStore>;
-export type AppState = ReturnType<AppStore['getState']>;
 export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<AppStore['getState']>;
 
-export const wrapper = createWrapper<AppStore>(makeStore, { debug: process.env.NODE_ENV === 'development' });
+// Next.js wrapper
+export const wrapper = createWrapper<AppStore>(makeStore, {
+  debug: process.env.NODE_ENV === 'development',
+});
