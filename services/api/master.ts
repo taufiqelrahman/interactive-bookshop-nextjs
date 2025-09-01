e0d927c75967ce88a010d64d9c523ee1b8b3aaba
Taufiq <taufiqelrahman65@gmail.com>
2025-09-01 20:43:20 +0700

    Refactor getOccupations method to use proper capitalization for occupation names to enhance consistency and readability

--- services/api/master.ts
+++ services/api/master.ts
@@ -26,12 +26,12 @@ export default class Products {
     return Promise.resolve({
       data: {
         data: [
-          { id: '1', name: 'doctor', description: 'Heals patients', page_count: 5, indonesia: 'Dokter' },
-          { id: '2', name: 'chef', description: 'Cooks food', page_count: 3, indonesia: 'Koki' },
-          { id: '3', name: 'teacher', description: 'Educates students', page_count: 4, indonesia: 'Guru' },
-          { id: '4', name: 'astronaut', description: 'Explores space', page_count: 4, indonesia: 'Astronot' },
-          { id: '5', name: 'pilot', description: 'Flies planes', page_count: 4, indonesia: 'Pilot' },
-          { id: '6', name: 'police', description: 'Maintains law and order', page_count: 4, indonesia: 'Polisi' },
+          { id: '1', name: 'Doctor', description: 'Heals patients', page_count: 5, indonesia: 'Dokter' },
+          { id: '2', name: 'Chef', description: 'Cooks food', page_count: 3, indonesia: 'Koki' },
+          { id: '3', name: 'Teacher', description: 'Educates students', page_count: 4, indonesia: 'Guru' },
+          { id: '4', name: 'Astronaut', description: 'Explores space', page_count: 4, indonesia: 'Astronot' },
+          { id: '5', name: 'Pilot', description: 'Flies planes', page_count: 4, indonesia: 'Pilot' },
+          { id: '6', name: 'Police', description: 'Maintains law and order', page_count: 4, indonesia: 'Polisi' },
         ] as Occupation[],
       },
     });
