31e676319bb55885765fbbad35b2e4c72be158fe
Taufiq <taufiqelrahman65@gmail.com>
2025-09-10 06:26:38 +0700

    fix: refactor Checkout methods to consistently handle Shopify availability checks

--- services/shopify/checkout.ts
+++ services/shopify/checkout.ts
@@ -11,28 +11,32 @@ export default class Checkout {
   }
 
   create(data?): Promise<any> {
-    // return this.adapter.checkout.create(data);
-    return Promise.resolve({
-      data: {
-        id: 'chk_123',
-        items: data?.items || [],
-        total: 100000,
-        status: 'created',
-        ...data,
-      },
-    });
+    if (!IS_SHOPIFY_AVAILABLE) {
+      return Promise.resolve({
+        data: {
+          id: 'chk_123',
+          items: data?.items || [],
+          total: 100000,
+          status: 'created',
+          ...data,
+        },
+      });
+    }
+    return this.adapter.checkout.create(data);
   }
 
   get(id): Promise<any> {
-    // return this.adapter.checkout.fetch(id);
-    return Promise.resolve({
-      data: {
-        id,
-        items: [{ id: 'item_1', name: 'Book 1', qty: 1, price: 100000 }],
-        total: 100000,
-        status: 'created',
-      },
-    });
+    if (!IS_SHOPIFY_AVAILABLE) {
+      return Promise.resolve({
+        data: {
+          id,
+          items: [{ id: 'item_1', name: 'Book 1', qty: 1, price: 100000 }],
+          total: 100000,
+          status: 'created',
+        },
+      });
+    }
+    return this.adapter.checkout.fetch(id);
   }
 
   addLineItems(checkoutId: ShopifyBuy.ID, lineItems: ShopifyBuy.CheckoutLineItemInput[]): Promise<ShopifyBuy.Checkout> {
@@ -52,26 +56,30 @@ export default class Checkout {
   }
 
   updateLineItems(id, data): Promise<any> {
-    // return this.adapter.checkout.updateLineItems(id, data);
-    return Promise.resolve({
-      data: {
-        id,
-        items: data.items || [],
-        total: 100000,
-        status: 'items updated',
-      },
-    });
+    if (!IS_SHOPIFY_AVAILABLE) {
+      return Promise.resolve({
+        data: {
+          id,
+          items: data.items || [],
+          total: 100000,
+          status: 'items updated',
+        },
+      });
+    }
+    return this.adapter.checkout.updateLineItems(id, data);
   }
 
   removeLineItems(id, itemId): Promise<any> {
-    // return this.adapter.checkout.removeLineItems(id, itemId);
-    return Promise.resolve({
-      data: {
-        id,
-        removedItemId: itemId,
-        status: 'item removed',
-      },
-    });
+    if (!IS_SHOPIFY_AVAILABLE) {
+      return Promise.resolve({
+        data: {
+          id,
+          removedItemId: itemId,
+          status: 'item removed',
+        },
+      });
+    }
+    return this.adapter.checkout.removeLineItems(id, itemId);
   }
 
   // addDiscount(id, code): Promise<any> {
