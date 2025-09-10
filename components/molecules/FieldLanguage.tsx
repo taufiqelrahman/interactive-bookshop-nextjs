b630acbc1a8c66ceec462a9d208b995f3ace23d4
Taufiq <taufiqelrahman65@gmail.com>
2025-09-11 06:23:57 +0700

    refactor: add style prop to FieldLanguageProps interface for improved styling flexibility

--- components/molecules/FieldLanguage.tsx
+++ components/molecules/FieldLanguage.tsx
@@ -10,6 +10,7 @@ interface FieldLanguageProps {
   register: (schema: any) => (ref: HTMLInputElement | null) => void;
   schema: any;
   defaultChecked?: string;
+  style: React.CSSProperties;
 }
 
 const FieldLanguage = (props: FieldLanguageProps) => {
