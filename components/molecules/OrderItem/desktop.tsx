37eb03412a259d89dbe935c0e43bc149d5a32cbb
Taufiq <taufiqelrahman65@gmail.com>
2025-09-26 00:46:31 +0700

    feat: enhance OrderItem component with responsive design and detailed item display

--- components/molecules/OrderItem/desktop.tsx
+++ components/molecules/OrderItem/desktop.tsx
@@ -4,9 +4,11 @@ import NumberFormat from 'react-number-format';
 
 import Capsule from 'components/atoms/Capsule';
 import Card from 'components/atoms/Card';
+import Divider from 'components/atoms/Divider';
 import appConfig from 'config';
 import { mapKeyValue } from 'lib/format-array';
 import { date } from 'lib/format-date';
+import { useResponsive } from 'lib/hooks/useResponsive';
 import { Order } from 'store/orders/types';
 
 import { previewImg } from './helper';
@@ -18,11 +20,106 @@ interface OrderItemProps extends Order {
 }
 
 const OrderItem = (props: OrderItemProps) => {
+  const { isMobile } = useResponsive();
   const { t } = useTranslation('page-orders');
   const lineItems = (props.line_items || []).map((item) => ({
     ...item,
     customAttributes: mapKeyValue(item.properties || []),
   }));
+
+  if (isMobile) {
+    return (
+      <div style={props.style}>
+        <Card variant="border--light,square--light">
+          <div className="c-order-item">
+            <div className="c-order-item__detail">
+              <div className="c-order-item__detail--top">
+                <div className="c-order-item__detail--top--left">
+                  {props.isSkeleton ? <Skeleton height={16} width={80} /> : props.name.replace('#', '')}
+                </div>
+                <div className="c-order-item__detail--top--right">
+                  {props.isSkeleton ? <Skeleton height={16} width={70} /> : date(props.created_at, 'DD/MM/YYYY')}
+                </div>
+              </div>
+              <div className="c-order-item__detail--middle">
+                <div>
+                  <div className="c-order-item__detail__occupation">
+                    {props.isSkeleton ? (
+                      <Skeleton height={24} width={180} />
+                    ) : (
+                      lineItems.map((item) => item.customAttributes.Name).join(', ')
+                    )}
+                  </div>
+                  <div className="c-order-item__detail__books">
+                    {props.isSkeleton ? <Skeleton height={19} width={70} /> : `${lineItems.length} ${t('books')}`}
+                  </div>
+                </div>
+                {props.isSkeleton ? (
+                  <Skeleton height={47} width={47} />
+                ) : (
+                  <div className="c-order-item__detail__image">
+                    <img src={previewImg(lineItems[0])} alt="item preview" />
+                  </div>
+                )}
+              </div>
+              <Divider style={{ borderColor: '#EFEEF4', margin: '3px 0 9px' }} />
+              <div className="c-order-item__detail--bottom">
+                <div className="c-order-item__detail__price">
+                  {props.isSkeleton ? (
+                    <Skeleton height={24} width={80} />
+                  ) : (
+                    <NumberFormat value={props.total_price} thousandSeparator={true} prefix={'Rp'} displayType="text" />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Card>
+        <style jsx>{`
+          .c-order-item {
+            @apply flex items-center;
+            padding: 14px 16px;
+            &__detail {
+              @apply w-full;
+              &--top {
+                @apply flex items-center justify-between font-opensans text-xs;
+                color: #898699;
+              }
+              &__image {
+                @apply overflow-hidden;
+                padding: 4px;
+                background: #efeef4;
+                width: 47px;
+                height: 47px;
+                border-radius: 6px;
+              }
+              &__occupation {
+                @apply font-semibold;
+                line-height: 24px;
+              }
+              &--middle {
+                @apply flex items-center justify-between;
+                margin: 13px 0;
+              }
+              &__books {
+                @apply font-opensans text-sm;
+                line-height: 19px;
+                margin-top: 3px;
+              }
+              &__value {
+                @apply font-opensans font-semibold;
+                line-height: 24px;
+              }
+              &__price {
+                @apply font-semibold;
+                line-height: 24px;
+              }
+            }
+          }
+        `}</style>
+      </div>
+    );
+  }
   return (
     <div style={props.style}>
       <Card variant="border">
