b5f871e3c978cb8d2deffd64d61d9bc3fd9b9ea4
Taufiq <taufiqelrahman65@gmail.com>
2025-10-10 06:11:35 +0700

    feat: implement CART mock data for cart creation

--- services/api/cart.ts
+++ services/api/cart.ts
@@ -1,5 +1,6 @@
 import { User } from 'store/users/types';
 import { AdapterObject } from './index';
+import { CART } from './__mocks__/cart';
 
 export default class Cart {
   adapter: AdapterObject;
@@ -26,56 +27,8 @@ export default class Cart {
     // return this.adapter.secure.post(`${this.basePath}`, data);
     return Promise.resolve({
       data: {
-        // ShopifyBuy.Checkout properties
+        ...CART,
         id: checkoutId,
-        completedAt: null,
-        createdAt: new Date().toISOString(),
-        currencyCode: 'USD',
-        email: '',
-        lineItems: [],
-        note: null,
-        order: null,
-        orderStatusUrl: null,
-        ready: true,
-        requiresShipping: true,
-        shippingAddress: null,
-        shippingLine: null,
-        subtotalPrice: { amount: 100000, currencyCode: 'USD' },
-        taxExempt: false,
-        taxesIncluded: false,
-        totalPrice: { amount: 100000, currencyCode: 'USD' },
-        totalTax: { amount: 0, currencyCode: 'USD' },
-        updatedAt: new Date().toISOString(),
-        webUrl: '',
-        discountApplications: [],
-        customAttributes: [],
-        userErrors: [],
-        completed: false,
-        paymentDue: { amount: 0, currencyCode: 'USD' },
-        phone: '',
-        presentmentCurrencyCode: 'USD',
-        shippingDiscountAllocations: [],
-        appliedGiftCards: [],
-        buyerIdentity: {
-          countryCode: null,
-          email: '',
-          phone: '',
-          customer: null,
-          deliveryAddressPreferences: [],
-        },
-        // Additional required properties
-        lineItemsSubtotalPrice: { amount: 100000, currencyCode: 'USD' },
-        paymentDueV2: { amount: 0, currencyCode: 'USD' },
-        subtotalPriceV2: { amount: 100000, currencyCode: 'USD' },
-        totalPriceV2: { amount: 100000, currencyCode: 'USD' },
-        totalTaxV2: { amount: 0, currencyCode: 'USD' },
-        // Cart properties
-        user_id: 1,
-        created_at: new Date().toISOString(),
-        updated_at: new Date().toISOString(),
-        deleted_at: '',
-        cart_items: [],
-        // User cart extension
         checkout_id: checkoutId,
       } as User['cart'],
     });
