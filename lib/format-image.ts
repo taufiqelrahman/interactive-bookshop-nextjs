4b0eac5a2d918f4c5ae351cc749da321c16b748c
Taufiq <taufiqelrahman65@gmail.com>
2025-10-20 16:51:49 +0700

    refactor: add type annotation to getPreviewUrl function for improved type safety

--- lib/format-image.ts
+++ lib/format-image.ts
@@ -1,4 +1,6 @@
-export const getPreviewUrl = (attributes) => {
+import { Character } from 'store/cart/types';
+
+export const getPreviewUrl = (attributes: Character): string => {
   const filePath = '/static/images/child';
   const { Gender, Age, Skin, Hair } = attributes;
   return `${filePath}/${Gender || 'boy'}/${Age || 'kid'}/${Hair || 'short'}/${Skin || 'light'}.png`;
